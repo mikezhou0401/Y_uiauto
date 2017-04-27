95df658ec341e6158300264f8e3f2990123750cf
chenggang <chenggang@zbj.com>
2017-04-27 14:36:49 +0800

    throw one exception

--- src/test/java/com/zhubajie/qa/testcase/TestPublishPipeline.java
+++ src/test/java/com/zhubajie/qa/testcase/TestPublishPipeline.java
@@ -13,19 +13,23 @@ import java.util.Map;
 @Listeners({com.zhubajie.report.ZBJReport.class,com.zhubajie.listener.TestngListener.class})
 public class TestPublishPipeline extends TestBase {
 	@Test(dataProvider="dataDriver",retryAnalyzer = com.zhubajie.listener.TestngRetry.class)
-	public void testPublishPipeline(Map<String, String> caseParam){
-		this.goTo(caseParam.get("url"));
-		LoginPage loginpage = new LoginPage(driver);
-		loginpage.login(caseParam.get("username"), caseParam.get("password"));
-		MenuPage menuPage = new MenuPage(driver);
-		menuPage.buildMgt();
-		PublishPipelinePage publishPipelinePage = new PublishPipelinePage(driver);
-		publishPipelinePage.publishPipeline();
-		publishPipelinePage.inputEngineName();
-		publishPipelinePage.query();
-		publishPipelinePage.devClick();
-		publishPipelinePage.inputTagName();
-		publishPipelinePage.queryStructure();
-		
+	public void testPublishPipeline(Map<String, String> caseParam) throws Exception{
+		try {
+			this.goTo(caseParam.get("url"));
+			LoginPage loginpage = new LoginPage(driver);
+			loginpage.login(caseParam.get("username"), caseParam.get("password"));
+			MenuPage menuPage = new MenuPage(driver);
+			menuPage.buildMgt();
+			PublishPipelinePage publishPipelinePage = new PublishPipelinePage(driver);
+			publishPipelinePage.publishPipeline();
+			publishPipelinePage.inputEngineName();
+			publishPipelinePage.query();
+			publishPipelinePage.devClick();
+			publishPipelinePage.inputTagName();
+			publishPipelinePage.queryStructure();
+		} catch (Exception e) {
+			throw new Exception("**********出错了，请检查代码！**********************");
+		}
+
 	}
 }
