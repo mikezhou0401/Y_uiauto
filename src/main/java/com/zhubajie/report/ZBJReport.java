c7e88e9e63940188e28a8cf457d377af769d86d2
chenggang <chenggang@zbj.com>
2017-05-02 16:34:30 +0800

    add two setting

--- src/main/java/com/zhubajie/report/ZBJReport.java
+++ src/main/java/com/zhubajie/report/ZBJReport.java
@@ -53,7 +53,7 @@ public class ZBJReport implements IReporter {
     private void insertReport(List<ITestResult> list){
         for (ITestResult result : list) {
             int costTime = Long.valueOf(result.getEndMillis()-result.getStartMillis()).intValue();
-            String sql = "INSERT INTO zhubajie_qa.qa_ui (id,className,methodName,`status`,time,execTime) VALUES(0,'"+result.getTestClass().getRealClass().getName()+"','"+result.getMethod().getMethodName()+"','"+this.getStatus(result.getStatus())+"',NOW(),"+costTime+","+ DateUtil.getYMD()+");";
+            String sql = "INSERT INTO zhubajie_qa.qa_ui (id,className,methodName,`status`,time,execTime) VALUES(0,'"+result.getTestClass().getRealClass().getName()+"','"+result.getMethod().getMethodName()+"','"+this.getStatus(result.getStatus())+"',NOW(),"+costTime+",'"+ DateUtil.getYMD()+"');";
             DBBusiness.update(sql);
         }
     }
